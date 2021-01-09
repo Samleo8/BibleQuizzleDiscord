0c3c38ba84c79e780bdc4653accfe69e0f52be7f
Samuel Leong <leowhiz@yahoo.com.sg>
2021-01-09 11:50:11 +0800

    Added templating

---
+++ commands/template.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+
+module.exports = {
+    name: 'REPLACEME',
+    description: "REPLACEME",
+    execute (msg, args) {
+        msg.reply("REPLACEME");
+        return;
+    },
+};
