93431529cf5f57c0edc2c9b915287e1f92454619
Samuel Leong <leowhiz@yahoo.com.sg>
2021-01-13 19:04:19 +0800

    Fix comments

--- constants.js
+++ constants.js
@@ -6,11 +6,14 @@ module.exports = {
     name: "constants"
 };
 
+// Channels where welcome message would be sent
 const welcomeChannels = ['quizzle', 'biblequizzle', 'game', 'games'];
 
+// Welcome message
 const welcomeMessage =
     `Welcome to Bible Quizzle, a fast-paced Bible trivia game similar to Quizzarium!\n\nTo begin the game, type ${Format.asCmdStr("start")} in the bot\'s private chat, or in the group. For more information and a list of all commands, type ${Format.asCmdStr("help")}`;
 
+// Help message when !help is sent
 const helpMessage =
     `Bible Quizzle is a fast-paced Bible trivia game. Once the game is started, the bot will send a question. Send in your open-ended answer and the bot will give you points for the right answer. The faster you answer, the more points you get! Each question has a 50 second timer, and hints will be given every 10 seconds. Alternatively, you can call for a ${Format.asCmdStr("hint")} but that costs points. Note that for all answers, numbers are in digit (0-9) form.\n\n` +
     `${Format.asCmdStr("start")} - Starts a new game.\n` +
@@ -23,9 +26,11 @@ const helpMessage =
     `${Format.asCmdStr("eggs")} - Hmm, what could this be?\n` +
     `${Format.asCmdStr("help")} - Displays this help message.\n`;
 
+// Quiz Categories
 const categories = ["All", "Old Testament", "New Testament", "Gospels", "Prophets", "Miracles", "Exodus",
     "Kings/Judges"];
 
+// Special RegEx stuff
 const regex = {
     alphanum: new RegExp("[A-Z0-9]", "gi"),
     non_alphanum: new RegExp("[^A-Z0-9]", "gi")
@@ -33,8 +38,10 @@ const regex = {
 
 // const ADMIN_ID = 413007985;
 
+// Maximum time before reaction wait timeout
 const maxTime = 5 * 60000; // NOTE: in milliseconds
 
+// Export all constants
 module.exports.cmdChar = cmdChar;
 module.exports.welcomeChannels = welcomeChannels;
 module.exports.welcomeMessage = welcomeMessage;
