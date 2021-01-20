a136938d4bc3aa258aabdb8999faa8e2910d3149
Samuel Leong <leowhiz@yahoo.com.sg>
2021-01-20 11:58:36 +0800

    Fix issue with cmd char for eggs text

--- commands/eggs/index.js
+++ commands/eggs/index.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const { cmdChar } = require("../../format");
 
 module.exports = {
     name: 'eggs',
@@ -6,7 +7,7 @@ module.exports = {
     execute (msg, args) {
         const attachment = new Discord.MessageAttachment('./img/egg.gif');
         const eggCaption =
-            "Congrats on finding your first easter egg!\n\nEaster eggs are fun secret commands, like /eggs, that will send cute photos or gifs like this one. They range from cute typos to random words and expressions.\n\nHappy hunting!\n";
+            `Congrats on finding your first easter egg!\n\nEaster eggs are fun secret commands, like ${cmdChar}eggs, that will send cute photos or gifs like this one. They range from cute typos to random words and expressions.\n\nHappy hunting!\n`;
 
         msg.reply(
             eggCaption,
